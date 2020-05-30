97802925fa012b2713149212801b40f982cd26b4
Dimas Nur Alif Rochmadtulloh <dimasnur.dev@gmail.com>
2020-05-30 09:07:15 +0700

    Initial commit

---
+++ app/src/main/java/com/dnar/dicodingsubmissionbfaa/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dnar.dicodingsubmissionbfaa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
