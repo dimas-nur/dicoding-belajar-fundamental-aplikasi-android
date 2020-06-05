9ab2c2cd8ef6bf59cf05066e9c8b113ca2da6c0f
Dimas Nur Alif Rochmadtulloh <dimasnur.dev@gmail.com>
2020-06-05 13:49:28 +0700

    Add function for get arguments when creating fragment

---
+++ app/src/main/java/com/dnar/dicodingsubmissionbfaa/data/util/BundleUtil.kt
@@ -0,0 +1,7 @@
+package com.dnar.dicodingsubmissionbfaa.data.util
+
+// Util for tag arguments; Keyword : Util
+
+/* --- Fragment --- */
+const val ARG_FRAGMENT_TYPE = "fragment_type"
+const val ARG_FRAGMENT_VALUE = "fragment_value"
--- app/src/main/java/com/dnar/dicodingsubmissionbfaa/ui/main/profile/follow/ProfileFollowFragment.kt
+++ app/src/main/java/com/dnar/dicodingsubmissionbfaa/ui/main/profile/follow/ProfileFollowFragment.kt
@@ -1,7 +1,10 @@
 package com.dnar.dicodingsubmissionbfaa.ui.main.profile.follow
 
+import android.os.Bundle
 import androidx.lifecycle.MutableLiveData
 import com.dnar.dicodingsubmissionbfaa.R
+import com.dnar.dicodingsubmissionbfaa.data.util.ARG_FRAGMENT_TYPE
+import com.dnar.dicodingsubmissionbfaa.data.util.ARG_FRAGMENT_VALUE
 import com.dnar.dicodingsubmissionbfaa.databinding.FragmentProfileFollowBinding
 import com.dnar.dicodingsubmissionbfaa.ui.base.BaseFragment
 
@@ -13,5 +16,17 @@ class ProfileFollowFragment :
         ProfileFollowViewModel::class.java
     override var title: MutableLiveData<String> = MutableLiveData("Profile")
 
+    companion object {
 
+        fun newInstance(type: String, userId: Int): ProfileFollowFragment {
+            val bundle = Bundle().apply {
+                putString(ARG_FRAGMENT_TYPE, type)
+                putInt(ARG_FRAGMENT_VALUE, userId)
+            }
+
+            return ProfileFollowFragment().apply {
+                arguments = bundle
+            }
+        }
+    }
 }
